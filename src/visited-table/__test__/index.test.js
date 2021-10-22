3c29f78df093561a70d46d8121080bf1734dc4c4
fishr-flash <fishr.flash@gmail.com>
2021-10-22 23:04:14 +0300

    all time is calculation

--- src/visited-table/__test__/index.test.js
+++ src/visited-table/__test__/index.test.js
@@ -1,4 +1,4 @@
-import {getMs} from "../utils";
+import {getMs, getTimeItems} from "../utils";
 
 describe('users page', () => {
     it('.setItem, choosing a different roleId', function () {
@@ -22,9 +22,28 @@ describe('users page', () => {
 
     it('parse days', function () {
 
-        const time = '2:30:02';
+        const time = '1 days 2:30:02';
         const allTimeMs = 910586000;
-        expect(allTimeMs + getMs(time)).toBe(time)
+        expect(allTimeMs + getMs(time)).toBe(1005988000)
+
+    })
+
+    it('getStringTime', function () {
+
+        const times =[
+            [ 1000 * 60, {"days": "0", "hours": "0", "minutes": "1", "seconds": 0} ],
+            [ 1000 * 60 * 60, {"days": "0", "hours": "1", "minutes": "0", "seconds": 0} ],
+            [1000 * 60 * 60 * 24,{"days": "1", "hours": "0", "minutes": "0", "seconds": 0} ],
+            [1000 * 60 * 60 * 24 + 1000,{"days": "1", "hours": "0", "minutes": "0", "seconds": 1} ],
+        ];
+        const allTimeMs = 910586000;
+
+        expect(getTimeItems(allTimeMs)).toStrictEqual({"days": "11", "hours": "13", "minutes": "56", "seconds": 26});
+
+        times.forEach((v) => {
+            expect(getTimeItems(v[0])).toStrictEqual(v[1]);
+        })
+
 
     })
 });
--- src/visited-table/utils.js
+++ src/visited-table/utils.js
@@ -1,59 +1,66 @@
 
 export const getMs = (timeString)=>{
 
+    const times = timeString.split( 'days');
+    const timePart = times[times.length - 1].trim();
     const dateString = '1970-01-01T';
     const utfPostfix = 'z';
-    const time = timeString.length === 7 ? `0${timeString}` : timeString;
+    const time = timePart.length === 7 ? `0${timePart}` : timePart;
     const date =  new Date(`${dateString}${time}${utfPostfix}`);
 
+    if( times.length > 1){
+
+        const day = new Date(0);
+        const daySeconds = day.setUTCHours( times[0].trim() * 24 );
+        return date.getTime() + daySeconds;
+    }
     return date.getTime();
 
-}
+};
+
+export const getTimeItems = (ms ) =>{
+    const sec = 1000;
+    const mn = sec * 60;
+    const hour = mn * 60;
+    const day = hour * 24;
+
+    const days = ( ms / day ).toFixed(0);
+    const hours = ( ( ms % day ) / hour ).toFixed(0);
+    const minutes = ( ( ( ms % day ) % hour ) / mn ).toFixed(0);
+    const seconds =  (( ( ms % day ) % hour ) % mn) / sec;
+
+    return {
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+
+};
 
 
 export const extractData = (data) => {
 
+    const startDate = new Date();
     let allTimeMs = 0;
     data.forEach((unit) => {
-
-        /////////////////////////////CONSOLE/////////////////////////////////////
-            ///TODO: Console log in the code "UTILS_JS" line 19
-            if( process && process.env.NODE_ENV === 'development' ){
-                console.group( 'Console log in the code "UTILS_JS" line 19' );
-                console.info( 'unit: ', unit );
-                console.info( 'unit.data: ', unit.data );
-                console.info( 'this: ', this );
-                //console.table( this );
-                console.groupEnd();
-            }
-        /////////////////////////////END CONSOLE/////////////////////////////////
         unit.data.forEach((unitData) =>{
-            /////////////////////////////CONSOLE/////////////////////////////////////
-                ///TODO: Console log in the code "UTILS_JS" line 31
-                if( process && process.env.NODE_ENV === 'development' ){
-                    console.group( 'Console log in the code "UTILS_JS" line 31' );
-                    console.info( 'unitData: ', unitData );
-                    console.info( 'unitData.duration_in: ', unitData.duration_in );
-                    console.info( 'getMs(unitData.duration_in): ', getMs(unitData.duration_in) );
-                    console.info( 'allTimeMs:  ', allTimeMs );
-                    console.info( 'this: ', this );
-                    //console.table( this );
-                    console.groupEnd();
-                }
-            /////////////////////////////END CONSOLE/////////////////////////////////
             allTimeMs += getMs(unitData.duration_in)
         })
     })
+
     /////////////////////////////CONSOLE/////////////////////////////////////
-    ///TODO: Console log in the code "INDEX_JS" line 18
-    if( process && process.env.NODE_ENV === 'development' ){
-        console.group( 'Console log in the code "INDEX_JS" line 18' );
-        console.info( 'data: ', data );
-        console.info( 'allTimeMs: ', allTimeMs );
-        console.info( 'this: ', this );
-        //console.table( this );
-        console.groupEnd();
-    }
+        ///TODO: Console log in the code "UTILS_JS" line 32
+        if( process && process.env.NODE_ENV === 'development' ){
+            console.group( 'Console log in the code "UTILS_JS" line 32' );
+            console.info( 'startDate - new Date(): ', startDate - new Date() );
+            console.info( 'this: ', this );
+            console.info( 'allTimeMs: ', allTimeMs );
+            console.info( 'getTimeItems( allTimeMs): ', getTimeItems( allTimeMs) );
+            //console.table( this );
+            console.groupEnd();
+        }
     /////////////////////////////END CONSOLE/////////////////////////////////
+
 };
 
