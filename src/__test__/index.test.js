0f145af7bb6ae78173fcf32a1f8c6627d30509ed
fishr.flash <fisher.flash@gmail.com>
2022-06-17 09:17:15 +0300

    заполнение дневника

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,19 +1,22 @@
+
 import {fullClone} from "../utils";
 import Smeta2 from "../smeta2";
 import Smeta from "../smeta";
 import React from "react";
 
-describe( 'src/root', () => {
+describe('src/root', () => {
     it('should utils', function () {
         expect(fullClone(null)).toBe(null)
     });
 
     it('should  smeta', function () {
         const sm = <div>
-            <Smeta />
-            <Smeta2 />
+            <Smeta/>
+            <Smeta2/>
         </div>;
 
+        // const sh = shallow( Smeta );
+        // expect(sh).toMatchSnapshot(sh);
         expect(sm).toMatchSnapshot(sm);
     });
 })
